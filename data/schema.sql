01041a45624b62a21e3d1f8f4ccd37f8b5f0c8c8
Artemis <wzyzhouyi@gmail.com>
2020-10-11 15:55:01 -0700

    created database structure

--- data/schema.sql
+++ data/schema.sql
@@ -3,6 +3,25 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- CREATE TABLE (
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
 
--- );
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY (id)
+);
