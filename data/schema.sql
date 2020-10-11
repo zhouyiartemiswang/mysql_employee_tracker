063213040e00d6457de78c6e1a3892cf4f56d8ee
Artemis <wzyzhouyi@gmail.com>
2020-10-11 16:22:44 -0700

    added data to database

--- data/schema.sql
+++ data/schema.sql
@@ -25,3 +25,30 @@ CREATE TABLE employee (
     manager_id INTEGER,
     PRIMARY KEY (id)
 );
+
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Engineering");
+INSERT INTO department (name) VALUES ("Finance");
+INSERT INTO department (name) VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer", 190000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Mike", "Chan", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Ashley", "Rogriguez", 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Kevin", "Tupik", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Malia", "Brown", 5);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Sarah", "Lourd", 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Tom", "Allen", 7, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Christian", "Eckenrode", 2, 2);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
--- index.js
+++ index.js
@@ -15,3 +15,5 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 
 });
+
+// connection.query("SELECT * FROM department")
